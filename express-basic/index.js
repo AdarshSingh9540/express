572a5cdcc36bfd8119fdd8aa3fe7f2316febdb24
Adarsh <adarsh9540984202@gmail.com>
2024-03-16 23:30:46 +0530

    add

--- express-basic/index.js
+++ express-basic/index.js
@@ -1,19 +1,47 @@
 const express = require("express");
-
-function calculateSum(n){
-    let sum =0;
-    for(let i=1;i<=n;i++){
-        sum+=i;
-    }
-    return sum;
-}
-
+const { format } = require("path");
 const app = express();
+// function calculateSum(n){
+//     let sum =0;
+//     for(let i=1;i<=n;i++){
+//         sum+=i;
+//     }
+//     return sum;
+// }
 
-app.get('/',function(req,res){
-    const n = req.query.n;
-    const sum = calculateSum(n);
-    res.send(sum.toString()) 
+const users = [{
+    name:"Adarsh",
+    
+        kidneys:[{
+            healthy:true
+      
+    }]
+}]
+
+
+// app.get('/',function(req,res){
+//     const n = req.query.n;
+//     const sum = calculateSum(n);
+//     res.send(sum.toString()) 
+// })
+
+app.get("/",function(req,res){
+    const Adarshkidneys = users[0].kidneys;
+    const noofkidneys = Adarshkidneys.length;
+    let noofhealthykidneys = 0;
+    for(let i=0; i<noofkidneys;i++){
+        if(Adarshkidneys[i].healthy){
+            noofhealthykidneys = noofhealthykidneys+1;
+        }
+    }
+
+    let noofunhealthykidneys = noofkidneys - noofhealthykidneys;
+    res.json({
+        noofkidneys,
+        noofhealthykidneys,
+        noofunhealthykidneys
+    })
 })
 
-app.listen(4000)
+
+app.listen(3000)
