d7466390157c62fc71a05f06c3539a46c7801a01
Adarsh <adarsh9540984202@gmail.com>
2024-03-16 20:07:53 +0530

    add it

--- express-basic/index.js
+++ express-basic/index.js
@@ -1,1 +1,19 @@
-``
+const express = require("express");
+
+function calculateSum(n){
+    let sum =0;
+    for(let i=1;i<=n;i++){
+        sum+=i;
+    }
+    return sum;
+}
+
+const app = express();
+
+app.get('/',function(req,res){
+    const n = req.query.n;
+    const sum = calculateSum(n);
+    res.send(sum.toString()) 
+})
+
+app.listen(4000)
